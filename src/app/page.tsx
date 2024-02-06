130383aea3547e3076bd34ff2e641bb099207c1a
Isaac Martinez <isaac@minnekdigital.com>
2024-02-05 22:32:48 -0400

    feat: add dark theme support and adjust primary color
    - Update global CSS variables for primary and dark themes
    - Add dark theme colors to Tailwind configuration
    - Implement dark theme elements in page component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,10 @@ export default function Home() {
   return (
     <main className="">
       <Button />
+      <div className="flex gap-10 p-10">
+        <div className="size-20 bg-primary"></div>
+        <div className="size-20 bg-dark"></div>
+      </div>
     </main>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -52,6 +52,10 @@ const config = {
           DEFAULT: "hsl(var(--card))",
           foreground: "hsl(var(--card-foreground))",
         },
+        dark: {
+          DEFAULT: "hsl(var(--dark))",
+          foreground: "hsl(var(--dark-foreground))",
+        },
       },
       borderRadius: {
         lg: "var(--radius)",
