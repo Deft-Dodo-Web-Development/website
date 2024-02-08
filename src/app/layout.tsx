8e01a6b3d20c0bc08cff914b96b1d9d131de2870
Isaac Martinez <isaac@minnekdigital.com>
2024-02-08 00:18:35 -0400

    feat(app/layout): add Header component to layout
    - Integrated Header component into the RootLayout
    - Adjusted main content margin to accommodate the header

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Header from "@/modules/common/widgets/header";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,7 +18,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${inter.className} bg-dark text-white`}>
-        <main className="container">{children}</main>
+        <Header />
+        <main className="container mt-28">{children}</main>
       </body>
     </html>
   );
