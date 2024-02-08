c3882a1208f63a75252588e4fbc177857c0d83ec
Isaac Martinez <isaac@minnekdigital.com>
2024-02-08 00:17:56 -0400

    feat(header): integrate Sheet component for mobile menu
    - Integrated Sheet component from @radix-ui/react-dialog for mobile menu
    - Replaced manual open/close state management with Sheet's onOpenChange
    - Updated HeaderMobileContent to use SheetContent for styling and layout

--- src/modules/common/components/header-mobile.tsx
+++ src/modules/common/components/header-mobile.tsx
@@ -1,5 +1,11 @@
 "use client";
 
+import {
+  Sheet,
+  SheetContent,
+  SheetTrigger,
+} from "@/modules/common/components/sheet";
+
 import { Menu, X } from "lucide-react";
 import { useState } from "react";
 import { HeaderCTA } from "./header-cta";
@@ -10,24 +16,27 @@ const HeaderMobileMenu = () => {
 
   return (
     <div className="md:hidden">
-      <button
-        onClick={() => setIsOpen(!isOpen)}
-        className="hover:scale-105"
-        title={isOpen ? "Close menu" : "Open menu"}
-        aria-label={isOpen ? "Close menu" : "Open menu"}
-      >
-        {!isOpen ? <Menu size={24} aria-hidden /> : <X size={24} aria-hidden />}
-      </button>
-      {isOpen && <HeaderMobileContent />}
+      <Sheet onOpenChange={(open) => setIsOpen(open)}>
+        <SheetTrigger asChild>
+          <button
+            className="hover:scale-105"
+            title="Open menu"
+            aria-label="Open menu"
+          >
+            {!isOpen && <Menu size={24} aria-hidden />}
+          </button>
+        </SheetTrigger>
+        <HeaderMobileContent />
+      </Sheet>
     </div>
   );
 };
 
 const HeaderMobileContent = () => (
-  <>
-    <HeaderNav />
+  <SheetContent className="md:hidden bg-dark max-w-xs pt-16 pb-40  items-center justify-between flex flex-col gap-20">
+    <HeaderNav className="flex-col justify-start" />
     <HeaderCTA />
-  </>
+  </SheetContent>
 );
 
 export { HeaderMobileMenu };
