6719e9444e287e03f9496384978a0985bfbcd87e
Isaac Martinez <isaac@minnekdigital.com>
2024-02-07 23:16:12 -0400

    feat(common/button): enhance button with link support and adjust padding
    - Introduced Link support to Button component for better integration with Next.js routing
    - Modified default button padding for improved design consistency

--- src/modules/common/components/button.tsx
+++ src/modules/common/components/button.tsx
@@ -4,6 +4,7 @@ import { cva, type VariantProps } from "class-variance-authority";
 
 import { cn } from "@/lib/utils";
 import { Icon, IconName } from "./icon";
+import Link from "next/link";
 
 const buttonVariants = cva(
   "inline-flex items-center justify-center whitespace-nowrap rounded-[100px] text-[16px] font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-primary focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50",
@@ -18,7 +19,7 @@ const buttonVariants = cva(
         ghost: "hover:bg-accent hover:text-accent-foreground",
       },
       size: {
-        default: "h-10 px-6 py-3",
+        default: "h-10 px-6 py-4",
         sm: "h-9 rounded-md px-3",
         lg: "h-11 rounded-md px-8",
         icon: "h-10 w-10",
@@ -37,44 +38,71 @@ const buttonVariants = cva(
   }
 );
 
-export interface ButtonProps
-  extends React.ButtonHTMLAttributes<HTMLButtonElement>,
-    VariantProps<typeof buttonVariants> {
-  asChild?: boolean;
-  icon?: IconName;
-}
+type WithLink =
+  | ({ href: string } & React.AnchorHTMLAttributes<HTMLAnchorElement>)
+  | ({ href?: never } & React.ButtonHTMLAttributes<HTMLButtonElement>);
+
+type ButtonProp =
+  | { icon: IconName; asChild?: never }
+  | { icon?: never; asChild?: boolean };
+
+export type ButtonProps = WithLink &
+  VariantProps<typeof buttonVariants> &
+  ButtonProp;
 
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
-  (
-    { className, variant, size, asChild = false, icon, children, ...props },
-    ref
-  ) => {
-    const Comp = asChild ? Slot : "button";
-
+  ({ className, variant, size, icon, children, ...props }, ref) => {
     if (icon) {
       return (
-        <button
+        <ButtonOrLink
           className={cn("gap-2", buttonVariants({ variant, size, className }))}
-          ref={ref}
+          ref={ref as React.Ref<WithLink>}
           {...props}
         >
           {children}
           <Icon name={icon} className="size-5" />
-        </button>
+        </ButtonOrLink>
+      );
+    }
+
+    if (!props.asChild) {
+      return (
+        <ButtonOrLink
+          className={cn(buttonVariants({ variant, size, className }))}
+          ref={ref as React.Ref<WithLink>}
+          {...props}
+        >
+          {children}
+        </ButtonOrLink>
       );
     }
 
     return (
-      <Comp
+      <Slot
         className={cn(buttonVariants({ variant, size, className }))}
         ref={ref}
         {...props}
       >
         {children}
-      </Comp>
+      </Slot>
     );
   }
 );
+
+const ButtonOrLink = React.forwardRef<WithLink, WithLink>((props, ref) => {
+  if (props.href) {
+    return <Link {...props} ref={ref as React.Ref<HTMLAnchorElement>} />;
+  }
+
+  return (
+    <button
+      {...(props as React.ButtonHTMLAttributes<HTMLButtonElement>)}
+      ref={ref as React.Ref<HTMLButtonElement>}
+    />
+  );
+});
+
+ButtonOrLink.displayName = "ButtonOrLink";
 Button.displayName = "Button";
 
 export { Button, buttonVariants };
