543dd2221ecc150429716818c7925ee9106bd53c
Isaac Martinez <isaac@minnekdigital.com>
2024-02-08 23:02:26 -0400

    feat: add dialog component and hero carousel for the home page
    - Implement a reusable dialog component with Radix UI for modal interactions.
    - Add a hero carousel component to the home page, featuring a video overlay that plays a video in a modal upon interaction.
    - Introduce a new 'hero-video' component for embedding video content in the carousel.
    - Utilize 'next/image' for optimized image handling and a 'useState' hook for managing the video play state.
    - Include assets and styles specific to the home page's hero carousel and video components.

--- src/modules/home/page/index.tsx
+++ src/modules/home/page/index.tsx
@@ -1,7 +1,12 @@
 import React from "react";
+import HeroCarousel from "../components/hero-carousel";
 
 const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+    <>
+      <HeroCarousel />
+    </>
+  );
 };
 
 export default HomePage;
