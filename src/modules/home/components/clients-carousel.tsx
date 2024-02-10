770b5a81ab77fd819285f53524dfc3985e3c0444
Isaac Martinez <isaac@minnekdigital.com>
2024-02-10 18:34:48 -0400

    feat(clients-carousel): add animation to client logos
    - Introduce 'motion.div' from Framer Motion for animated logo appearance
    - Implement initial, whileInView, and transition properties for smooth animations

--- src/modules/home/components/clients-carousel.tsx
+++ src/modules/home/components/clients-carousel.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { motion } from "framer-motion";
 import Autoplay from "embla-carousel-autoplay";
 import Image, { StaticImageData } from "next/image";
 import { Carousel, CarouselContent, CarouselItem } from "@components/carousel";
@@ -34,13 +35,25 @@ const ClientsCarousel: React.FC<ClientsCarouselProps> = ({ images }) => {
             key={index}
             className="sm:basis-1/2 md:basis-1/3 pl-36 xl:basis-1/5 flex items-center justify-center"
           >
-            <Image
-              src={image.src}
-              width={image.width}
-              height={image.height}
-              alt={image.alt}
-              className="object-contain max-w-[215px] max-h-[48px] w-full"
-            />
+            <motion.div
+              className="relative"
+              initial={{ opacity: 0, bottom: -20, scale: 0.8 }}
+              whileInView={{ opacity: 1, bottom: 0, scale: 1 }}
+              viewport={{ once: true }}
+              transition={{
+                duration: 0.5,
+                delay: 0.1 * index,
+                ease: "easeOut",
+              }}
+            >
+              <Image
+                src={image.src}
+                width={image.width}
+                height={image.height}
+                alt={image.alt}
+                className="object-contain max-w-[215px] max-h-[48px] w-full"
+              />
+            </motion.div>
           </CarouselItem>
         ))}
       </CarouselContent>
