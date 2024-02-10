ec527ead122d379b392a87ba70350dd228dc6e1b
Jose Genao <j.genao@minnekdigital.com>
2024-02-09 21:49:14 -0300

    feat(common/badge): add badge component
    - Added new Badge component to common components.
    - Defined styling and variants for the badge.

---
+++ src/modules/common/components/badge.tsx
@@ -0,0 +1,47 @@
+import * as React from "react";
+import { cva, type VariantProps } from "class-variance-authority";
+
+import { cn } from "@/lib/utils";
+
+const badgeVariants = cva(
+  "inline-flex items-center justify-center text-center rounded-full border py-4 px-8 text-2xl transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2",
+  {
+    variants: {
+      variant: {
+        default:
+          "border-transparent bg-white text-dark bg-primary hover:bg-primary/80",
+        secondary:
+          "border-transparent bg-secondary text-secondary-foreground hover:bg-secondary/80",
+        destructive:
+          "border-transparent bg-destructive text-destructive-foreground hover:bg-destructive/80",
+        outline: "text-foreground text-white border-white-16 ",
+      },
+      size: {
+        default: "py-4 px-8 text-2xl",
+        base: "py-2 px-4 text-base",
+        sm: "py-2 px-4 text-lg",
+        md: "py-3 px-6 text-xl",
+        lg: "py-5 px-10 text-3xl",
+      },
+    },
+    defaultVariants: {
+      variant: "default",
+      size: "default",
+    },
+  }
+);
+
+export interface BadgeProps
+  extends React.HTMLAttributes<HTMLDivElement>,
+    VariantProps<typeof badgeVariants> {}
+
+function Badge({ className, variant, size, ...props }: BadgeProps) {
+  return (
+    <div
+      className={cn(badgeVariants({ variant, size }), className)}
+      {...props}
+    />
+  );
+}
+
+export { Badge, badgeVariants };
