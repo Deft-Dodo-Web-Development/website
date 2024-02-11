e7795505aa5db3847db34f68b8058c9c3e4143f2
Isaac Martinez <isaac@minnekdigital.com>
2024-02-11 00:47:00 -0400

    feat(about): add Team widget showcasing team members
    - Introduce Profile component to detail team member information
    - Include images and bios for team members in Team widget
    - Update AboutPage to feature Team widget, enhancing page content

--- src/modules/about/page/index.tsx
+++ src/modules/about/page/index.tsx
@@ -1,6 +1,7 @@
 import { Separator } from "@components/separator";
 import AboutUsHeading from "../widgets/heading";
 import Content from "../widgets/content";
+import Team from "../widgets/team";
 
 const AboutPage = () => {
   return (
@@ -9,6 +10,7 @@ const AboutPage = () => {
       <Separator className="mb-28 mt-14" container />
       <Content container />
       <Separator className="mt-28 mb-8" container />
+      <Team container />
     </>
   );
 };
