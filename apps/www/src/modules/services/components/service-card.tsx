e86332cf0c35a131828aa1abd3965da0136155fe
German Rosario <ray_jr@hotmail.es>
2024-03-08 00:54:06 -0400

    feat(www): DDT-35 add service card primary variation

--- apps/www/src/modules/services/components/service-card.tsx
+++ apps/www/src/modules/services/components/service-card.tsx
@@ -9,6 +9,7 @@ export type ServiceCardProps = {
   description: string;
   slug: string;
   icon: IconName;
+  variant?: "default" | "primary";
 };
 
 const ServiceCard: React.FC<ServiceCardProps> = ({
@@ -16,12 +17,25 @@ const ServiceCard: React.FC<ServiceCardProps> = ({
   description,
   icon,
   slug,
+  variant = "default",
 }) => {
   const { locale } = useAppData();
+  const settings = {
+    default: {
+      background: "bg-white-8",
+      text: "text-primary",
+      buttonVariant: "outline",
+    },
+    primary: {
+      background: "bg-primary",
+      text: "text-white",
+      buttonVariant: "default",
+    },
+  }
 
   return (
-    <section className="p-12 bg-white-8 rounded-lg space-y-8">
-      <Icon name={icon} className="size-16 text-primary" />
+    <section className={settings[variant].background + " p-8 rounded-lg space-y-8 h-full"}>
+      <Icon name={icon} className={settings[variant].text + " size-16"} />
       <div className="space-y-3">
         <h3 className="font-bold text-xl xl:text-2xl text-white sm:min-h-14 lg:min-h-16">
           {title}
@@ -30,7 +44,7 @@ const ServiceCard: React.FC<ServiceCardProps> = ({
       </div>
       <Button
         icon="ArrowUpRight"
-        variant="outline"
+        variant={settings[variant].buttonVariant}
         className="text-sm"
         href={`/${locale}/services/${slug}`}
       >
