018969641a7ee29592a5beaa7d4813927fc3c1fe
Isaac Martinez <isaac@minnekdigital.com>
2024-03-09 02:05:02 -0400

    feat(www): integrate environment-specific Strapi URL in Next.js config
    - Integrated dynamic Strapi URL configuration using environment variables.
    - Utilized dotenv for environment variable management.
    - Configured remotePatterns in Next.js image optimization to adapt to variable Strapi URLs.

--- apps/www/next.config.js
+++ apps/www/next.config.js
@@ -1,11 +1,19 @@
+const dotenv = require('dotenv');
+dotenv.config();
+
+const url = process.env.NEXT_PUBLIC_STRAPI_URL;
+const strapi = new URL(url);
+const port = url.split(":")[2]?.replaceAll("/", "");
+
 /** @type {import('next').NextConfig} */
 module.exports = {
   transpilePackages: [],
   images: {
-    remotePatterns: [{
-      protocol: 'http',
-      hostname: '127.0.0.1',
-      port: '1337',
-    }]
+    remotePatterns: [
+      {
+        protocol: strapi.protocol.slice(0, -1),
+        hostname: strapi.hostname,
+        port: port,
+      }]
   }
 };
