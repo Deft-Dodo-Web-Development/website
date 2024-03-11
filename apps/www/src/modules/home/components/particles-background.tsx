7df2e00293f3d77de45b1f9bf3a42f48b16e90ed
Isaac Martinez <isaac@minnekdigital.com>
2024-03-10 20:10:36 -0400

    refactor(www): DDT-48 update React component implementations - Modified marquee.tsx to include key props in children mapping - Updated particles-background.tsx for optimal particle engine initialization and rendering adjustments - Enhanced hero.tsx to use Suspense for lazy loading the ParticlesBackground component

--- apps/www/src/modules/home/components/particles-background.tsx
+++ apps/www/src/modules/home/components/particles-background.tsx
@@ -1,113 +1,95 @@
 "use client";
 
-import { useEffect, useMemo, useState } from "react";
 import Particles, { initParticlesEngine } from "@tsparticles/react";
-import {
-    type Container,
-    type ISourceOptions
-} from "@tsparticles/engine";
 import { loadSlim } from "@tsparticles/slim";
+import { useState, useEffect } from "react";
 
 const App = () => {
-    const [init, setInit] = useState(false);
+  const [init, setInit] = useState(false);
 
-    useEffect(() => {
-        initParticlesEngine(async (engine) => {
-            await loadSlim(engine);
-        }).then(() => {
-            setInit(true);
-        });
-    }, []);
+  useEffect(() => {
+    initParticlesEngine(async (engine) => {
+      await loadSlim(engine);
+    }).then(() => {
+      setInit(true);
+    });
+  }, []);
 
-    const particlesLoaded = async (container?: Container): Promise<void> => {
-        console.log(container);
-    };
-
-    const options: ISourceOptions = useMemo(
-        () => ({
-            fullScreen: {
-                enable: false,
-                zIndex: -1,
+  if (!init) return null;
+  return (
+    <Particles
+      options={{
+        fullScreen: {
+          enable: false,
+          zIndex: -1,
+        },
+        background: {
+          color: {
+            value: "transparent",
+          },
+        },
+        fpsLimit: 30,
+        interactivity: {
+          events: {
+            onClick: {
+              enable: false,
             },
-            background: {
-                color: {
-                    value: "transparent",
-                },
+            onHover: {
+              enable: false,
             },
-            fpsLimit: 30,
-            interactivity: {
-                events: {
-                    onClick: {
-                        enable: false
-                    },
-                    onHover: {
-                        enable: false
-                    },
-                },
-                modes: {
-                    push: {
-                        quantity: 4,
-                    },
-                    repulse: {
-                        distance: 200,
-                        duration: 0.4,
-                    },
-                },
+          },
+          modes: {
+            push: {
+              quantity: 4,
             },
-            particles: {
-                color: {
-                    value: "#ffffff80",
-                },
-                links: {
-                    color: "#ffffff",
-                    distance: 150,
-                    enable: true,
-                    opacity: 0.5,
-                    width: 1,
-                },
-                move: {
-                    direction: "none",
-                    enable: true,
-                    outModes: {
-                        default: "out",
-                    },
-                    random: false,
-                    speed: 2,
-                    straight: false,
-                },
-                number: {
-                    density: {
-                        enable: true,
-                    },
-                    value: 80,
-                },
-                opacity: {
-                    value: 0.5,
-                },
-                shape: {
-                    type: "circle",
-                },
-                size: {
-                    value: { min: 1, max: 5 },
-                },
+            repulse: {
+              distance: 200,
+              duration: 0.4,
             },
-            detectRetina: true,
-        }),
-        [],
-    );
-
-    if (init) {
-        return (
-            <Particles
-                id="tsparticles"
-                particlesLoaded={particlesLoaded}
-                options={options}
-                className="h-full"
-            />
-        );
-    }
-
-    return <></>;
+          },
+        },
+        particles: {
+          color: {
+            value: "#ffffff80",
+          },
+          links: {
+            color: "#ffffff",
+            distance: 150,
+            enable: true,
+            opacity: 0.5,
+            width: 1,
+          },
+          move: {
+            direction: "none",
+            enable: true,
+            outModes: {
+              default: "out",
+            },
+            random: false,
+            speed: 2,
+            straight: false,
+          },
+          number: {
+            density: {
+              enable: true,
+            },
+            value: 80,
+          },
+          opacity: {
+            value: 0.5,
+          },
+          shape: {
+            type: "circle",
+          },
+          size: {
+            value: { min: 1, max: 5 },
+          },
+        },
+        detectRetina: true,
+      }}
+      className="h-full"
+    />
+  );
 };
 
 export default App;
