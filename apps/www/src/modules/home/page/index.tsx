c5672d3bb769b6440e919598100f0c849fc64060
José Tomás Genao <60038867+josetgenao26@users.noreply.github.com>
2024-03-16 16:54:48 -0300

    feat: DDT-44 create brands marquee
    * feat: create brands marquee section

--- apps/www/src/modules/home/page/index.tsx
+++ apps/www/src/modules/home/page/index.tsx
@@ -11,6 +11,7 @@ import KeyFactsSection from "../widgets/key-facts";
 import FeaturedServices from "../widgets/featured-services";
 import Testimonials from "../widgets/testimonials";
 import OurProcess from "../widgets/our-process";
+// import BrandsMarquee from "../widgets/brands-marquee";
 import Team from "../widgets/team";
 import Contact from "../widgets/contact";
 import { Metadata } from "next";
@@ -54,6 +55,8 @@ const HomePage = async () => {
             return <HeroCarousel key={index} {...content} />;
           case "home.clients":
             return <Clients key={index} {...content} />;
+          // case "home.brands-marquee":
+          //   return <BrandsMarquee key={index} {...content} />;
           case "home.about-us":
             return <AboutUs key={index} {...content} />;
           case "home.projects":
@@ -95,7 +98,6 @@ const HomePage = async () => {
             return null;
         }
       })}
-
       <FooterSetup variant="basic" />
     </>
   );
