e791c071b79666c4ac5a4490438e030b0044f890
Isaac Martinez <isaac@minnekdigital.com>
2024-03-17 16:27:47 -0400

    feat(www): DDT-55 Add baseFetch utility function
    - Implement baseFetch function in utilities to handle API requests.
    - Include handling for query string parameters and authorization headers.

---
+++ apps/www/src/modules/common/utilities/fetch.ts
@@ -0,0 +1,53 @@
+import { AppConfig } from "@/config/app.config";
+import { config } from "dotenv";
+import qs from "qs";
+config();
+
+export type BaseFetchParams = {
+  entity: string;
+  populate?: string[];
+  by?: {
+    [key: string]: string | number;
+  };
+};
+
+export async function baseFetch<TData>({
+  entity,
+  populate,
+  by,
+}: BaseFetchParams): Promise<TData> {
+  const id = by?.id;
+  const filter = id ? { filter: by } : {};
+  const query = qs.stringify({
+    populate,
+    ...filter,
+  });
+  const baseUrl = `${AppConfig.strapi.url}/api/${entity}`;
+  const url = id ? `${baseUrl}/${id}?${query}` : `${baseUrl}?${query}`;
+
+  const request = await fetch(url, {
+    method: "GET",
+    headers: {
+      // eslint-disable-next-line turbo/no-undeclared-env-vars
+      Authorization: `Bearer ${process.env.STRAPI_ACCESS_TOKEN}`,
+    },
+    next: { revalidate: 120 },
+  });
+
+  const data = await request.json();
+
+  if (by && data?.data) {
+    return {
+      ...data,
+      data: (() => {
+        if (data.data instanceof Array) {
+          return data.data[0];
+        }
+
+        return data.data;
+      })(),
+    };
+  }
+
+  return data as TData;
+}
