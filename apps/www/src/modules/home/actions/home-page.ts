4e62ddf1fbb0c4e3e9cb5fad183c92417ae17cfc
Isaac Martinez <isaac@minnekdigital.com>
2024-03-22 21:01:37 -0400

    fix(www): Simplify fetching home page data using baseFetch utility
    - Refactored getHomePageData function to utilize the baseFetch utility for cleaner and more maintainable code
    - Removed direct fetch call and manual query string construction in favor of a more abstracted fetchData function
    - Improved code readability and reusability by centralizing fetch logic

--- apps/www/src/modules/home/actions/home-page.ts
+++ apps/www/src/modules/home/actions/home-page.ts
@@ -8,50 +8,47 @@ import {
 } from "../types/response";
 import qs from "qs";
 import { AppConfig } from "@/config/app.config";
+import { baseFetch } from "@/modules/common/utilities/fetch";
 
 config();
 
-export async function getHomePageData() {
-  const query = qs.stringify({
-    populate: [
-      "pageContent",
-      "pageContent.video",
-      "pageContent.video.overlay",
-      "pageContent.start",
-      "pageContent.start_content",
-      "pageContent.projects",
-      "pageContent.services",
-      "pageContent.list.image",
-      "pageContent.heading",
-      "pageContent.projects.image",
-      "pageContent.services.icon",
-      "pageContent.featured",
-      "pageContent.steps",
-      "pageContent.testimonials",
-      "pageContent.testimonials.identity",
-      "pageContent.testimonials.identity.picture",
-      "pageContent.testimonials.identity.company_logo",
-      "pageContent.members",
-      "pageContent.members.picture",
-      "pageContent.members.social",
-      "pageContent.contact-us",
-      "pageContent.experience",
-      "pageContent.button",
-    ],
-  });
-  const request = await fetch(
-    `${AppConfig.strapi.url}/api/home-page?${query}`,
-    {
-      method: "GET",
-      headers: {
-        Authorization: `Bearer ${process.env.STRAPI_ACCESS_TOKEN}`,
-      },
-      next: { revalidate: 60 },
-    }
-  );
+const fetchData = function <T>(filter?: { [key: string]: string | number }) {
+  const populate = [
+    "pageContent",
+    "pageContent.video",
+    "pageContent.video.overlay",
+    "pageContent.start",
+    "pageContent.start_content",
+    "pageContent.projects",
+    "pageContent.services",
+    "pageContent.list.image",
+    "pageContent.heading",
+    "pageContent.projects.image",
+    "pageContent.services.icon",
+    "pageContent.featured",
+    "pageContent.steps",
+    "pageContent.testimonials",
+    "pageContent.testimonials.identity",
+    "pageContent.testimonials.identity.picture",
+    "pageContent.testimonials.identity.company_logo",
+    "pageContent.members",
+    "pageContent.members.picture",
+    "pageContent.members.social",
+    "pageContent.contact-us",
+    "pageContent.experience",
+    "pageContent.button",
+  ];
 
-  const data = await request.json();
-  return data as HomePageServerResponse;
+  return baseFetch<T>({
+    entity: "home-page",
+    populate,
+    by: filter,
+  });
+};
+
+export async function getHomePageData() {
+  const data = await fetchData<HomePageServerResponse>();
+  return data;
 }
 
 export async function getSeoPageData() {
