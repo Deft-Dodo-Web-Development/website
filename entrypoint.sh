fb50d64ed328710e877afc1e7cb0a5116a0813a6
Isaac Martinez <isaac@minnekdigital.com>
2024-03-23 00:34:12 -0400

    feat(www): Enhance entrypoint script for dynamic environment handling
    - Changed target environment file to .env.production for clarity in production settings
    - Added validation to ensure NEXT_PUBLIC_STRAPI_URL is set before proceeding
    - Implemented search and replace for APP_NEXT_PUBLIC_STRAPI_URL placeholder in Next.js build output
    - Included a message indicating the start of the Next.js application for better log visibility

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 
-echo "" > /app/apps/www/.env
+echo "" > /app/apps/www/.env.production
 printenv | grep NEXT_PUBLIC_ >> /app/apps/www/.env
 
+test -n "$NEXT_PUBLIC_STRAPI_URL"
+find /app/apps/www/.next \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i "s#APP_NEXT_PUBLIC_STRAPI_URL#$NEXT_PUBLIC_STRAPI_URL#g"
+
+echo "Starting Nextjs"
 exec "$@"
