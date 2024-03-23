634b0578fe4ce9d98e1bc3287fb7900145badac6
Isaac Martinez <isaac@minnekdigital.com>
2024-03-22 20:03:19 -0400

    fix(www): DDT-70 Adjust Next.js config for dynamic Strapi URL

--- apps/www/next.config.js
+++ apps/www/next.config.js
@@ -6,7 +6,7 @@ const strapi_url = process.env.NEXT_PUBLIC_STRAPI_URL || "";
 const contain_port = /:\d+/.test(strapi_url);
 
 // Yes I know, this is a bad way to do it, but I'm lazy :)
-const url = contain_port ? strapi_url : port ? `${strapi_url}:${_port}` : strapi_url;
+const url = contain_port ? strapi_url : _port ? `${strapi_url}:${_port}` : strapi_url;
 
 const strapi = new URL(url);
 const port = url.split(":")[2]?.replaceAll("/", "");
