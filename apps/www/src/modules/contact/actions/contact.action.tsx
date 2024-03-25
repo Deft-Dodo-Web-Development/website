12745f3b47e3ad7d5eccc85b6a313b219ecbc752
Isaac Martinez <isaac@minnekdigital.com>
2024-03-24 23:23:51 -0400

    feat(www): DDT-74 Enhance contact action with sender email configuration and notification
    - Added environment variable for configurable sender email in contact actions.
    - Implemented additional email notification to the sender for new contact messages.

--- apps/www/src/modules/contact/actions/contact.action.tsx
+++ apps/www/src/modules/contact/actions/contact.action.tsx
@@ -12,17 +12,23 @@ export async function ContactAction(data: {
   message: string;
 }) {
   const resend = new Resend(process.env.RESEND_API_KEY!);
+  const sender = process.env.RESEND_SENDER_EMAIL || "isaac@zitdevs.com"; // This is my email. Should be changed to the email of the company
 
   const sent = await resend.emails.send({
-    from: "isaac@zitdevs.com",
+    from: sender,
     to: data.email,
     subject: `Thanks for reaching out, ${data.name}!`,
     react: <DeftDodoConfirmation name={data.name} />,
   });
 
-  console.log(sent);
+  const sent2 = await resend.emails.send({
+    from: sender,
+    to: sender,
+    subject: `New message from ${data.name}`,
+    text: `Name: ${data.name}\nEmail: ${data.email}\nMessage: ${data.message}`,
+  });
 
-  if (sent.error) return { status: 400, message: sent.error };
+  if (sent.error || sent2.error) return { status: 400, message: sent.error };
 
   return { status: 200, message: "Success" };
 }
