ae9eaed0681c339490c72362c6a5181a7250227e
German Rosario <ray_jr@hotmail.es>
2024-03-28 18:36:10 -0400

    feat(www): add validation for portfolio page
    DDT-81

--- apps/www/src/modules/work/components/project-card.tsx
+++ apps/www/src/modules/work/components/project-card.tsx
@@ -5,6 +5,7 @@ import Image from "next/image";
 import { StrapiResponse, Media } from "@/modules/common/types/common";
 import Link from "next/link";
 import { AppConfig } from "@/config/app.config";
+import { PlaceholderIcon } from "@/modules/common/components/placeholder-icon";
 
 export type ProjectCardProps = {
   image: StrapiResponse<Media>;
@@ -25,17 +26,23 @@ const ProjectCard: React.FC<ProjectCardProps> = (card) => {
       transition={{ ease: "backInOut" }}
     >
       <Link href={link}>
-        <Image
-          src={`${AppConfig.strapi.url}${card.image.data.attributes.url}`}
-          alt={card.image.data.attributes.alternativeText || card.title}
-          width={750}
-          height={440}
-          className="rounded-xl"
-        />
-        <div className="flex flex-col mt-6">
-          <h3 className="text-2xl">{card.title}</h3>
-          <p className="text-[16px] text-white-56">{card.summary}</p>
-        </div>
+        {card.image.data ? (
+          <Image
+            src={`${AppConfig.strapi.url}${card.image.data.attributes.url}`}
+            alt={card.image.data.attributes.alternativeText || card.title}
+            width={750}
+            height={440}
+            className="rounded-xl"
+          />
+        ) : (<PlaceholderIcon className="size-full rounded-xl" />)
+        }
+
+        {card.title || card.summary ? (
+          <div className="flex flex-col mt-6">
+            {card.title ? (<h3 className="text-2xl">{card.title}</h3>) : null}
+            {card.summary ? (<p className="text-[16px] text-white-56">{card.summary}</p>) : null}
+          </div>
+        ) : null}
       </Link>
     </motion.div>
   );
