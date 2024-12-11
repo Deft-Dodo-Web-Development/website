c95af26639d4c36243808b909b8f0c181239fedd
DESKTOP-GF1J8F9\German <ray_jr@hotmail.es>
2024-12-10 21:26:26 -0400

    chore(www): add pricing information section to ServicesPage

--- apps/www/src/modules/services/page/index.tsx
+++ apps/www/src/modules/services/page/index.tsx
@@ -4,6 +4,7 @@ import ServiceList from "../widgets/service-list";
 import FooterSetup from "@utilities/footer-setup";
 import { getAllServiceAction } from "../actions/service.action";
 import { notFound } from "next/navigation";
+import { Container } from "@components/container";
 
 const ServicesPage = async () => {
   const services = await getAllServiceAction();
@@ -20,6 +21,14 @@ const ServicesPage = async () => {
   return (
     <>
       <ServicesHeading container />
+      <Container enabled={true} asChild>
+        <section className="w-full my-4 md:my-6 lg:my-8 content uppercase text-center">
+          <p className="text-xl">Our pricing structure is <span className="text-primary">simple</span>, <span className="text-primary">straightforward</span>, and <span className="text-primary">transparent</span>.
+          <br/>
+          We charge a flat rate of $75 / hour across all of our services.
+          </p>
+        </section>
+      </Container>
       <Separator className="mb-12 mt-6" container />
       <ServiceList container services={services.data || []} />
       <FooterSetup variant="complex" />
